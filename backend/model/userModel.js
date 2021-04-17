bc49ccb34589e366793ce1fab12486fe82a4b042
pmoragas <pmoragas@gmail.com>
2021-04-17 14:21:31 +0200

    register and login able from back

---
+++ backend/model/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 1024
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
